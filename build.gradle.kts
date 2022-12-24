e4ec0008ab122be2ea1dd69416ad68726952dba6
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2022-12-24 19:45:45 +0600

    added jvm target on scraper module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
-    kotlin("multiplatform") version "1.6.10" apply false
-    kotlin("plugin.serialization") version "1.6.10" apply false
+    kotlin("multiplatform") version "1.7.20" apply false
+    kotlin("plugin.serialization") version "1.7.20" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
-    id("org.jetbrains.dokka") version "1.6.10" apply false
+    id("org.jetbrains.dokka") version "1.7.20" apply false
     id("com.vanniktech.maven.publish") version "0.22.0" apply false
     id("org.jetbrains.kotlinx.kover") version "0.6.1" apply false
 }
