1fcaf0dc0c3b799c689b071ca73dbd1f9f9d0dda
Jamshed Alam Qaderi <54829362+JamshedAlamQaderi@users.noreply.github.com>
2022-12-24 20:28:50 +0600

    Fix/support all platform (#8)
    * added jvm target on scraper module
    
    * added jvm module int the scraper
    
    * changed group id

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
-    kotlin("multiplatform") version "1.6.10" apply false
-    kotlin("plugin.serialization") version "1.6.10" apply false
+    kotlin("multiplatform") version "1.7.20" apply false
+    kotlin("plugin.serialization") version "1.7.20" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
-    id("org.jetbrains.dokka") version "1.6.10" apply false
+    id("org.jetbrains.dokka") version "1.7.20" apply false
     id("com.vanniktech.maven.publish") version "0.22.0" apply false
     id("org.jetbrains.kotlinx.kover") version "0.6.1" apply false
 }
@@ -12,7 +12,7 @@ plugins {
 val projectVersion: String? by project
 
 allprojects {
-    group = "com.jamshedalamqaderi"
+    group = "com.jamshedalamqaderi.anview"
     version = projectVersion?.replaceFirst("v", "", true) ?: "0.0.1-SNAPSHOT"
     apply {
         plugin("org.jetbrains.kotlinx.kover")
--- scraper/build.gradle.kts
+++ scraper/build.gradle.kts
@@ -14,23 +14,6 @@ android {
     defaultConfig {
         minSdk = 21
         targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
@@ -41,6 +24,8 @@ kotlin {
         }
         publishLibraryVariants("release")
     }
+
+    jvm()
 
     sourceSets {
         val commonMain by getting {
